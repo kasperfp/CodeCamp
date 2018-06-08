9fc3ad5c69c50e20e767a76aa39c068230d654da
Kasper Petersen <kasperfp@gmail.com>
2018-06-08 21:49:54 +0200

    ThemePark model

---
+++ src/CubrisProject/CubrisProject/Models/ThemePark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CubrisProject.Models
+{
+    public class ThemePark
+    {
+        public string ThemeParkId { get; set; }
+        public string ThemeParkName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
