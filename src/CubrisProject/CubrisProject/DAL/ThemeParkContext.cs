be275dd7da82affb23f9161f3e9ead7e103b1215
Kasper Petersen <kasperfp@gmail.com>
2018-06-09 10:00:05 +0200

    Created DAL folder for contexts.

---
+++ src/CubrisProject/CubrisProject/DAL/ThemeParkContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CubrisProject.Models
+{
+    public class ThemeParkContext : DbContext
+    {
+        public ThemeParkContext(DbContextOptions<ThemeParkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ThemePark> ThemeParks { get; set; }
+    }
+}
