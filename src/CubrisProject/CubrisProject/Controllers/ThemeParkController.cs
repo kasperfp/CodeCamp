12b9479263710bb50aa31b980f8aa71b4ee32958
Kasper Petersen <kasperfp@gmail.com>
2018-06-09 09:59:36 +0200

    Created ThemePark controller.

---
+++ src/CubrisProject/CubrisProject/Controllers/ThemeParkController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CubrisProject.Models;
+
+namespace CubrisProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ThemePark")]
+    public class ThemeParkController : ControllerBase
+    {
+        private readonly ThemeParkContext _context;
+
+        public ThemeParkController(ThemeParkContext context)
+        {
+            _context = context;
+
+            if (_context.ThemeParks.Count() == 0)
+            {
+                _context.ThemeParks.Add(new ThemePark {
+                    ThemeParkName = "Tivoli",
+                    Latitude = 55.673684,
+                    Longitude = 12.568147
+                });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
