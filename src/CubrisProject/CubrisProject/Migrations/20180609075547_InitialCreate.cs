830d76ade3ba69b22eea049fa4035c02c9778985
Kasper Petersen <kasperfp@gmail.com>
2018-06-09 09:58:27 +0200

    SQLlite Migrations setup.

---
+++ src/CubrisProject/CubrisProject/Migrations/20180609075547_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CubrisProject.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ThemeParks",
+                columns: table => new
+                {
+                    ThemeParkId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ThemeParkName = table.Column<string>(nullable: true),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThemeParks", x => x.ThemeParkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ThemeParks");
+        }
+    }
+}
