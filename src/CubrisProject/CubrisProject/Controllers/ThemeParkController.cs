a809b286f1d9c125a588de2402b865714fff31ca
Kasper Petersen <kasperfp@gmail.com>
2018-06-09 14:03:53 +0200

    Create, delete and update ThemePark.

--- src/CubrisProject/CubrisProject/Controllers/ThemeParkController.cs
+++ src/CubrisProject/CubrisProject/Controllers/ThemeParkController.cs
@@ -17,34 +17,65 @@ namespace CubrisProject.Controllers
         public ThemeParkController(ThemeParkContext context)
         {
             _context = context;
-
-            if (_context.ThemeParks.Count() == 0)
-            {
-                _context.ThemeParks.Add(new ThemePark {
-                    ThemeParkName = "Tivoli",
-                    Latitude = 55.673684,
-                    Longitude = 12.568147
-                });
-                _context.SaveChanges();
-            }
         }
 
         [HttpGet("{id}", Name = "GetThemePark")]
         public ThemePark GetById(int id)
         {
-            var item = _context.ThemeParks.Find(id);
-            if (item == null)
+            var themePark = _context.ThemeParks.Find(id);
+            if (themePark == null)
             {
                 return null;
             }
-            return item;
+            return themePark;
         }
-
 
         [HttpGet("[action]")]
         public IEnumerable<ThemePark> GetAll()
         {
             return _context.ThemeParks.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ThemePark themePark)
+        {
+            _context.ThemeParks.Add(themePark);
+
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetThemePark", new { id = themePark.ThemeParkId }, themePark);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var themePark = _context.ThemeParks.Find(id);
+            if (themePark == null)
+            {
+                return NotFound();
+            }
+
+            _context.ThemeParks.Remove(themePark);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] ThemePark item)
+        {
+            var themePark = _context.ThemeParks.Find(id);
+            if (themePark == null)
+            {
+                return NotFound();
+            }
+
+            themePark.ThemeParkName = item.ThemeParkName;
+            themePark.Longitude = item.Longitude;
+            themePark.Latitude = item.Latitude;
+
+            _context.ThemeParks.Update(themePark);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
