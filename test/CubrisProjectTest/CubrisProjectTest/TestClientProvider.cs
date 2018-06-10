823b0b01caca73058cde866de68d96c796e8c194
Kasper Petersen <kasperfp@gmail.com>
2018-06-10 15:55:06 +0200

    Test client provider

---
+++ test/CubrisProjectTest/CubrisProjectTest/TestClientProvider.cs
@@ -0,0 +1,22 @@
+﻿using CubrisProject;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CubrisProjectTest
+{
+    public class TestClientProvider
+    {
+        public HttpClient Client { get; set; }
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = server.CreateClient();
+        }
+    }
+}
