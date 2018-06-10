352190c7c9cfe9d9ce1aca7924fd31fbcd99d0fb
Kasper Petersen <kasperfp@gmail.com>
2018-06-10 13:39:08 +0200

    Loading theme parks.

--- src/CubrisProject/CubrisProject/ClientApp/app/components/home/home.component.ts
+++ src/CubrisProject/CubrisProject/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { ThemeParkService } from '../../services/theme-park.service';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+    themeParks: any;
+
+    constructor(private http: Http, private themeParService: ThemeParkService) { }
+
+    ngOnInit(): void {
+        this.themeParService.getAll().subscribe(res => {
+            console.log("res: " + res.json());
+            this.themeParks = res.json();
+        }, err => { console.log("err: " + err); });
+    }
+
+    showThemePark(themePark: any) {
+        console.log(themePark.themeParkId);
+        console.log(themePark.latitude);
+        console.log(themePark.longitude);
+    }
 }
