ae07af25f6563a0d6937380b8c61d2645e0196fd
Kasper Petersen <kasperfp@gmail.com>
2018-06-10 12:59:23 +0200

    Created theme park service with GET req.

---
+++ src/CubrisProject/CubrisProject/ClientApp/app/services/theme-park.service.ts
@@ -0,0 +1,10 @@
+﻿import { Injectable, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+export class UserService {
+    constructor(private http: Http) {}
+
+    getAll() {
+        return this.http.get('http://localhost:54241/api/ThemeParks/GetAll');
+    }
+}
